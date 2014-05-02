53f69d72d63f0266d990a8a4f2273fadce59c38d
Lochlan McBride <lochlan.mcbride@bigtincan.com>
2014-05-02 10:37:10 +1000

    fix for 'NoneType' error

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Exports saved highlights from the Kobo ereader database to a CSV
 
 ## Usage
 Place KoboReader.sqlite in the same directory and execute export.py
+
+### Tested with:
+- Python 2.7.4
--- export.py
+++ export.py
@@ -5,16 +5,20 @@ import csv
 conn = sqlite3.connect('KoboReader.sqlite')
 c = conn.cursor()
 
-# Execute SQL
+# Get DB version
+version = c.execute('SELECT version FROM DbVersion').fetchone()[0]
+
+# Get highlights data
 highlights = c.execute('SELECT VolumeID, Text, DateCreated FROM Bookmark').fetchall()
 
 # Close database
 c.close()
 
 # Encode data
-encoded = [[s.encode('utf8') for s in t] for t in highlights]
+encoded = [[s.encode('utf8') for s in t if s is not None] for t in highlights]
 
-print (('Exporting %s Higlights...') % (len(highlights)))
+print(('DB Version: %s') % (version))
+print(('Exporting %s Higlights...') % (len(highlights)))
 
 # Write CSV
 writer = csv.writer(open('highlights.csv', 'wb'))
